34b3842b863524e3e0cb7c0319fd9b000c53a618
solomonbarayev <sbarayev2@gmail.com>
2022-08-01 16:34:10 +0300

    Updates

---
+++ precache-manifest.802e8bab74a434f1b1d6c9d2853ed402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55802eccd057df5c6b0dc19b4cbd9662",
+    "url": "/react-grocery-list-tracker/index.html"
+  },
+  {
+    "revision": "6fdd4a6db843066607f8",
+    "url": "/react-grocery-list-tracker/static/css/main.cdcc939d.chunk.css"
+  },
+  {
+    "revision": "81dea452a5f125a9c972",
+    "url": "/react-grocery-list-tracker/static/js/2.4896f6c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-grocery-list-tracker/static/js/2.4896f6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdd4a6db843066607f8",
+    "url": "/react-grocery-list-tracker/static/js/main.f8e1ab78.chunk.js"
+  },
+  {
+    "revision": "34d1fe73a85a6f6351f3",
+    "url": "/react-grocery-list-tracker/static/js/runtime-main.27dfdc68.js"
+  }
+]);
